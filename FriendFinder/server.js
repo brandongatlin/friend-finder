58013eac9e0f4cbc9103beeab4364ed53e600421
brandongatlin <brandongatlin1981@me.com>
2017-12-17 11:36:15 -0600

    moving

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var nosniff = require('dont-sniff-mimetype');
 
 var app = express();
 
+app.use(nosniff());
+
+
 var dirname = "localhost:";
 var PORT = 3000;
+
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(PORT, function() {
   console.log("listening on port 3000");
@@ -15,8 +21,9 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + '/app/public/home.html'));
 });
 
-app.get("/survey", function(req, res) {
+app.get("/survey.html", function(req, res) {
   res.sendFile(path.join(__dirname + '/app/public/survey.html'));
+  // res.send("survey is laded");
 });
 
 // app.post("/api/add", function(req, res) {
