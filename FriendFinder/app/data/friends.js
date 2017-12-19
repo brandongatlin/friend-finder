b40fd109f01210f964d17615ba4f0a7a514ae267
brandongatlin <brandongatlin1981@me.com>
2017-12-18 20:45:21 -0600

    added dummy friend data

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,12 +1,89 @@
-// var peeps = [
-//   {
-//     "name": $("#name-input").val().trim(),
-//     "photo": $("#img-input").val(),
-//     "scores": [
-//
-//     ]
-//   }
-// ];
+var peeps = [{
+    "name": "Fry",
+    "photo": "https://www.wired.com/images_blogs/underwire/2010/06/fry_660.jpg",
+    "scores": [
+      1,
+      2,
+      3,
+      4,
+      5,
+      2,
+      0,
+      4,
+      2,
+      1
+    ]
+  },
+
+  {
+    "name": "Lela",
+    "photo": "http://www.behindthevoiceactors.com/_img/chars/turanga-leela-futurama-92.5.jpg",
+    "scores": [
+      5,
+      2,
+      1,
+      2,
+      5,
+      2,
+      1,
+      4,
+      2,
+      3
+    ]
+  },
+
+  {
+    "name": "Zoidberg",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/4/4a/Dr_John_Zoidberg.png",
+    "scores": [
+      5,
+      5,
+      5,
+      4,
+      5,
+      1,
+      2,
+      4,
+      2,
+      5
+    ]
+  },
+
+  {
+    "name": "Bender",
+    "photo": "https://vignette.wikia.nocookie.net/himym/images/5/51/Bender.jpg/revision/latest?cb=20130506083200",
+    "scores": [
+      5,
+      5,
+      3,
+      4,
+      5,
+      2,
+      3,
+      4,
+      2,
+      5
+    ]
+  },
+
+  {
+    "name": "Amy",
+    "photo": "http://www.behindthevoiceactors.com/_img/chars/amy-wong-futurama-64.8.jpg",
+    "scores": [
+      1,
+      2,
+      3,
+      1,
+      1,
+      2,
+      2,
+      1,
+      2,
+      3
+    ]
+  }
+
+];
 
 
 
